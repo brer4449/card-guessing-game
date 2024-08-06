61cea8921dee7121c7c9974c48b1699f653a5126
Brendan Erickson <berickson92@Gmail.com>
2024-08-06 10:59:17 -0400

    added game play area

--- src/App.js
+++ src/App.js
@@ -10,18 +10,32 @@ function App() {
           Hunt the Ace
         </p>
       </header>
-      <div class="card-container">
-        <div class="card">
-          <div class="card-inner">
-            <div class="card-front">
-              <img src={ace} alt="ace of clubs"/>
-            </div>
-            <div class="card-back">
-              <img src={cardBack} alt="back of card"/>
-            </div>
+      <main>
+        <div class="card-container">
+          <div class="card-pos-a">
+
           </div>
+          <div class="card-pos-b">
+
+          </div>
+          <div class="card-pos-c">
+
+          </div>
+          <div class="card-pos-d">
+
+          </div>
+          {/* <div class="card">
+            <div class="card-inner">
+              <div class="card-front">
+                <img src={ace} alt="ace of clubs"/>
+              </div>
+              <div class="card-back">
+                <img src={cardBack} alt="back of card"/>
+              </div>
+            </div>
+          </div> */}
         </div>
-      </div>
+      </main>
     </div>
   );
 }
