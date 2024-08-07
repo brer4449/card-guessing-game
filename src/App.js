f74abddf2c7430d8e371cd6df3b2dc555b817c7e
Brendan Erickson <berickson92@Gmail.com>
2024-08-07 18:36:09 -0400

    added game area for cards

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
-import './App.css';
-import ace from './ac.png';
-import cardBack from './card-back.png';
+import "./App.css";
+import images from "./utils/importImages";
+import cardBack from "./assets/cards/card-back.png";
 
 function App() {
+  const cardNames = ["ac.png", "jh.png", "kc.png", "qd.png"];
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hunt the Ace
-        </p>
+        <p>Hunt the Ace</p>
       </header>
       <main>
         <div class="card-container">
-          <div class="card-pos-a">
-
-          </div>
-          <div class="card-pos-b">
-
-          </div>
-          <div class="card-pos-c">
-
-          </div>
-          <div class="card-pos-d">
-
-          </div>
-          {/* <div class="card">
-            <div class="card-inner">
-              <div class="card-front">
-                <img src={ace} alt="ace of clubs"/>
-              </div>
-              <div class="card-back">
-                <img src={cardBack} alt="back of card"/>
+          {cardNames.map((cardName, idx) => (
+            <div key={cardName} className={`card-pos-${idx}`}>
+              <div class="card">
+                <div class="card-inner">
+                  <div class="card-front">
+                    <img src={images[cardName]} alt={cardName} />
+                  </div>
+                  <div class="card-back">
+                    <img src={cardBack} alt="back of card" />
+                  </div>
+                </div>
               </div>
             </div>
-          </div> */}
+          ))}
         </div>
       </main>
     </div>
